5f1e712587fbdd82f24e060b34b9f1f94a74c5e4
Javier Aguilar <javieragustinaguilar@gmail.com>
2024-05-22 18:21:47 -0300

    Se creo la estructura del Porfolio y se agregaron las dependencias

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <header>
+     
+     </header>
+     <main>
+
+     </main>
+     <footer>
+
+     </footer>
+    </>
+  )
+}
+
+export default App
